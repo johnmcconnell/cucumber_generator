c5fa3d6f373a7cfa4a5d18c3a5b6910de768db96
johnmcconnell <johnnyillinois@gmail.com>
2014-12-06 21:23:31 -0600

    fix given show

--- lib/generators/feature/templates/step_definitions/show_given_steps.rb
+++ lib/generators/feature/templates/step_definitions/show_given_steps.rb
@@ -1,4 +1,4 @@
-Given(/^I am on the <%= generator.send :table_name %> page$/) do
+Given(/^I am on the <%= generator.send :file_name %> page$/) do
   visit(<%= generator.file_name %>_path(<%= generator.file_name %>))
 end
 
