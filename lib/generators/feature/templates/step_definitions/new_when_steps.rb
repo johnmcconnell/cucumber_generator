baf38de0f8334c5f0313815006e1d0f8ec8c1468
johnmcconnell <johnnyillinois@gmail.com>
2014-12-06 21:36:03 -0600

    add when step

--- lib/generators/feature/templates/step_definitions/new_when_steps.rb
+++ lib/generators/feature/templates/step_definitions/new_when_steps.rb
@@ -2,3 +2,7 @@ When(/^I enter new <%= generator.file_name %> info$/) do
   enter_form(new_<%= generator.file_name %>_params)
 end
 
+When(/^I enter invalid new <%= generator.file_name %> info$/) do
+  enter_form(new_<%= generator.file_name %>_params)
+end
+
