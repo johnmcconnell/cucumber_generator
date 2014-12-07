20670ef4d9f33746c066bd66d8295c5f8b76d308
johnmcconnell <johnnyillinois@gmail.com>
2014-12-06 21:27:58 -0600

    add when edit step

--- lib/generators/feature/templates/step_definitions/edit_when_steps.rb
+++ lib/generators/feature/templates/step_definitions/edit_when_steps.rb
@@ -2,3 +2,7 @@ When(/^I enter edit <%= generator.file_name %> info$/) do
   enter_form(edited_<%= generator.file_name %>_params)
 end
 
+When(/^I enter invalid edit <%= generator.file_name %> info$/) do
+  enter_form(invalid_edited_<%= generator.file_name %>_params)
+end
+
