4d22874d62822cb4437efe43c410e303f56c63e7
johnmcconnell <johnnyillinois@gmail.com>
2014-12-08 23:20:54 -0600

    add invalid methods

--- lib/generators/feature/templates/step_definitions/edit_methods.rb
+++ lib/generators/feature/templates/step_definitions/edit_methods.rb
@@ -2,3 +2,7 @@ def edited_<%= generator.file_name %>_params
   pending
 end
 
+def invalid_edited_<%= generator.file_name %>_params
+  pending
+end
+
--- lib/generators/feature/templates/step_definitions/new_methods.rb
+++ lib/generators/feature/templates/step_definitions/new_methods.rb
@@ -2,3 +2,7 @@ def new_<%= generator.file_name %>_params
   pending
 end
 
+def invalid_new_<%= generator.file_name %>_params
+  pending
+end
+
